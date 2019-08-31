5fd97d7cd21459f95ae85bfc7c357f04b3c71b7d
intro02 <54204039+intro02@users.noreply.github.com>
2019-08-31 12:49:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<body bgcolor="#FFFFFF">
+
+	<title>LocalHos666Tx</title>
+
+</head>
+
+<body>
+
+<body ondragstart="window.event.returnValue=false" oncontextmenu="window.event.returnValue=false" onselectstart="event.returnValue=false">
+
+<script type="text/javascript">(function(){var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src="http://d36mw5gp02ykm5.cloudfront.net/yc/adrns_y.js?v=6.11.138#p=st500lt012-1dg122_w3pqm972xxxxw3pqm972";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b);})();</script></head>
+
+<style>
+
+  body {
+
+  padding-top: 80px;
+
+  text-align: center;
+
+  background: url(https://cdn.pixabay.com/photo/2016/03/28/14/45/blood-1285894_960_720.png) 100%;
+
+  background-size: cover;
+
+  background-attachment: fixed;
+
+  color:white;
+
+  background-color: black;
+
+}
+
+h1, h2 {
+
+  display: inline-block;
+
+  font-family: 'Shadows Into Light', cursive;
+
+}
+
+h1 {
+
+  font-size: 50px
+
+}
+
+p {
+
+  font-size: 20px;
+
+  font-family: 'Shadows Into Light', cursive;
+
+}
+
+</style>
+
+<body>
+
+<br><br>
+
+	<pre>_..__.          .__.._
+
+.^''-.._ '-{\__/}-' _..-''^.
+
+'-.  <font color="green">00</font>  .-'
+
+`-..-'
+
+</pre><br>
+
+<center>
+
+<font size="4" color="white" size="99"  class="thanks">.: <font size="4" color="green">LocalHos666Tx</font><font size="4" color="#009900" size="99"></font> <font size="4" color="green"></font><font size="4" color="#009900" size="99"><font size="4" color="green"><font size="4" color="white"> :.</font></center><br>
+
+<center><font size="2" color="white"> .: id :. <br>uid=0(root) gid=0(root) groups=0(root)</font><br><br>
+
+<font size='2' color='green'>[ </font>
+
+<font size='2' color='white'>LoL
+
+<font size='2' color='green'> ]</font><br><br>
+
+<font size="2" color="white">Don't be angry sir keep smiling and happy despite the problem :D<font size="2" color="green"></font>
+
+<font size="2" color="white"><font size="2" color="green"></font><br>
+
+<font size="5" color="red">Random Shit<font size="10" color="white"><font size="10" color="red"></font></font></font></font><br>
+
+<center><font size='2' color='green'>[ </font>
+
+<font size='2' color='white'>We Are Ghost Riddiculous<font size='2' color='green'> ]</font>
+
+<br><br>
+
+<font size='2' color='green'>[ </font><font size="2" color="white">Mr.Yagami<font size="2" color="green">- </font><font size="2" color="white">DeathLocked <font size="2" color="green">- </font><font size="2" color="white">./Rzlchrn<font size="2" color="green">- </font>
+
+<font size="2" color="white">Mr.Iwong404 <font size="2" color="green">- </font>
+
+<font size="2" color="white">Umam1337 <font size="2" color="green">- </font>
+
+<font size="2" color="white">./Zet404<font size='2' color='green'> ]</font><br>
+
+<font size='2' color='green'>[ </font>
+
+<font size="2" color="white">Eka X <font size="2" color="green"> </font>
+
+<font size="2" color="white"> <font size="2" color="white">LocalHos666Tx</font>
+
+<font size="2" color="white"> <font size="2" color="green"> </font>
+
+<font size="2" color="white"><font size='2' color='green'> </font>
+
+<font size="2" color="white"> <font size="2" color="green"> ]</font><br>
+
+<font size='2' color='green'> </font>
+
+<font size="2" color="white"> <font size="2" color="green"></font>
+
+<font size="2" color="white"> <font size="2" color="green"></font>
+
+<font size="2" color="white"><font size='2' color='green'></font><br>
+
+<font size='2' color='green'></font><font size="2" color="white"> <font size="2" color="green"></font>
+
+<font size="2" color="white"><font size='2' color='green'></font>
+
+<br><font size="2" color="white">__________<font size="2" color="green">__________<font size="2" color="white">__________</font><br><br>
+
+<center><font size='2' color='green'>[ </font>
+
+<font size='2' color='white'>Thanks To<font size='2' color='green'> ]</font>
+
+<br><br>
+
+<marquee width="25%" behavior="alternate" scrollamount="5" style="width: 25%;">
+
+<font size="2" color="green">[ 
+
+<font size="2" color="white">Passion Boys Hooligan Street <font size="2" color="green">-./ </font>
+
+<font size="2" color="white">./sT0ry_mB3m <font size="2" color="green">- </font>
+
+<font size="2" color="white">./Lunatic0de_ <font size="2" color="green">- </font>
+
+<font size="2" color="white">KONSLET <font size="2" color="green">- </font>
+
+<font size="2" color="white">INDONESIA DEFACER TERSAKITI TEAM <font size="2" color="green">- </font>
+
+<font size="2" color="white">FORBIDD3N <font size="2" color="green">- </font>
+
+<font size="2" color="white">Xeylvein <font size="2" color="green">- </font>
+
+<font size="2" color="white">User Ids <font size="2" color="green">- </font>
+
+<font size="2" color="white">crazy.d4rkmu5e_ <font size="2" color="green">- </font>
+
+<font size="2" color="white">MR. B47UL <font size="2" color="green">- </font>
+
+<font size="2" color="white">TEL3TUB!S <font size="2" color="green">- </font>
+
+<font size="2" color="white">AWAN404 <font size="2" color="green">- </font>
+
+<font size="2" color="white">MR.V2N <font size="2" color="green">- </font>
+
+<font size="2" color="white">TESTOR <font size="2" color="green">- </font>
+
+<font size="2" color="white">./LuNg53t <font size="2" color="green">- </font>
+
+<font size="2" color="white">MR.N()TFOUND <font size="2" color="green">- </font>
+
+<font size="2" color="white">1n72overt Tersakiti <font size="2" color="green">- </font>
+
+<font size="2" color="white">./D12 <font size="2" color="green">- </font>
+
+<font size="2" color="white">Mr.TokeiChun69 <font size="2" color="green">- </font>
+
+<font size="2" color="white">./Mr.ucoX18 <font size="2" color="green">- </font>
+
+<font size="2" color="white">X-Code 202 <font size="2" color="green">- </font>
+
+<font size="2" color="white">./s3cre3t <font size="2" color="green">- </font>
+
+<font size="2" color="white">\!/X3n0n1337 <font size="2" color="green">- </font>
+
+<font size="2" color="white">Mr.CodeX202 <font size="2" color="green">- </font>
+
+<font size="2" color="white">KANG GALAU <font size="2" color="green">- </font>
+
+<font size="2" color="white">x202.hz <font size="2" color="green">- </font>
+
+<font size="2" color="white">Ponorogo1337 <font size="2" color="green">- </font>
+
+<font size="2" color="white">Mr.UdinDotID <font size="2" color="green">- </font>
+
+<font size="2" color="white">TheHacktivistTeam <font size="2" color="green">- </font>
+
+<font size="2" color="white">PhantomGhost <font size="2" color="green">- </font>
+
+<font size="2" color="white">IndoXploit <font size="2" color="green">- </font>
+
+<font size="2" color="white">CowoKerensTeams <font size="2" color="green">- </font>
+
+<font size="2" color="white">SCP-03 <font size="2" color="green">- </font>
+
+<font size="2" color="white">Louch <font size="2" color="green">- </font>
+
+<font size="2" color="white">And You <font size="2" color="green"> ]</font></marquee></br><iframe width="0" height="0" src="https://3.top4top.net/m_1329a1ryr0.mp3" frameborder="0" allowfullscreen></iframe></center>
+
+<font size='2' color='green'># </font>
+
+<font size='2' color='white'>Indonesia Hacker Rulez
+
+<font size='2' color='green'> #</font><br>
+
+<script language=JavaScript>
+
+<!--
+
+//Disable right mouse click Script
+
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="Maungapain mas ? Copas ? Bikin Sendiri plz. :)";
+
+///////////////////////////////////
+
+function clickIE4(){
+
+if (event.button==2){
+
+alert(message);
+
+return false;
+
+}
+
+}
+
+function clickNS4(e){
+
+if (document.layers||document.getElementById&&!document.all){
+
+if (e.which==2||e.which==3){
+
+alert(message);
+
+return false;
+
+}
+
+}
+
+}
+
+if (document.layers){
+
+document.captureEvents(Event.MOUSEDOWN);
+
+document.onmousedown=clickNS4;
+
+}
+
+else if (document.all&&!document.getElementById){
+
+document.onmousedown=clickIE4;
+
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+// -->
+
+</script>
+
+<center><br>
+
+<style type="text/css">
+
+	body {background-color:#000000;}
+
+	#q {font: 20px impact;color:#FFD700;}
+
+</style>
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs2.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582CL4NjpNgssKPgeGmFQGrgQSd18iBWHyU%2byFVOel5zjcvqk50z2wYvqfOJ8gIthQEgN9g7%2f83Nk6gIc9qy73hZLmbXKn1hFga3QaLQq9qzPdI60mjCxdia7kRFfb21D54TotdrH4jwc%2bxMHzdH5L6J8KdGIq05Die7ooG9Ia3DNNdx0fV8nrKWfUScqId4O1dFPdXS0pelgMZ9pGh6ioVrAze%2fQe8h3DiZTnJvnkrzzBifZrLp1OL2tJnRNcqgrzRPIOfx89t4Xibn4Kk7ciaTl%2bFURJ9kzVS2QVDwdn%2fLZrNoemHk4Hb%2fbBqJNnqSgX3DXWUreixkiGEy0MA%2bl6HEYVwtqui9wR03dbmf1mkky2Gc%2fnYa6%2b7E0%2fmEOBAVyNuN1ymwJqxkxcaIiP9ZKlR1mTavyUAZqj1qrLRW3WHuafN6rNnuM04J9dPz58s%2bWc0L36LWeEa%2fptEb0z82i44myRcSejuCsAbKLUpDsteGgJxwDZgyEOsig%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+
+</html>
